c89eaff26d7179d4f0420176d60918e2746760e5
Malik Alhanouti <50356599+mlkia@users.noreply.github.com>
2022-02-16 13:12:55 +0100

    Class user

---
+++ Interactive_Saga/Interactive_Saga/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_Saga
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public int Diamonds { get; set; } = 0;
+        public int NumberOfCorrectAnswers { get; set; } = 0;
+        public int NumberOfWrongAnswers { get; set; } = 0;
+        public int Level { get; set; }= 1;
+
+    }
+}
