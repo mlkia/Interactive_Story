59f7b8195863557ee2c763fb575ef205383e471d
Josefin Persson <josefinp94@gmail.com>
2022-02-16 10:06:52 +0100

    Meny, program.cs samt helperklass för meddelanden

--- Interactive_Saga/Interactive_Saga/MainMenu.cs
+++ Interactive_Saga/Interactive_Saga/MainMenu.cs
@@ -1,8 +1,55 @@
-﻿using System;
+﻿using Interactive_Saga;
+using System;
 
 public class MainMenu
 {
-	public MainMenu()
+    /// <summary>
+    /// The message helper instance
+    /// </summary>
+    MessageHelper messageHelper = new();
+
+    /// <summary>
+    /// Runs the menu.
+    /// </summary>
+    public void RunMenu(/* User user, Universe universe*/)  
 	{
+        
+        while(/* user.WinOverBoss < 3 */ true ) // så länge man inte hunnit vinna över alla 3 bossar
+        {
+            messageHelper.MainMenuMessage();
+
+            string userMenuInput = Console.ReadLine();
+
+            int menuInputInt = 0;
+            int.TryParse(userMenuInput, out menuInputInt);
+
+            if ( menuInputInt > 4 || menuInputInt < 1)
+            {
+                messageHelper.MenuErrorMessage();
+            }
+
+            switch (menuInputInt)
+            {
+                    case 1:
+                    Console.Clear();
+                    // kör spel-metoden
+                    break;
+                    case 2:
+                    Console.Clear();
+                    // kör show stats-metoden
+                    break ;
+                    case 3:
+                    Console.Clear();
+                    messageHelper.RulesOfGame();
+                    break;
+                    case 4:
+                    // user.WinOverBoss = 3;
+                    break;
+                    default:
+                    messageHelper.MenuErrorMessage();
+                    break;
+
+            }
+        }
 	}
 }
---
+++ Interactive_Saga/Interactive_Saga/MessageHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_Saga
+{
+    public class MessageHelper
+    {
+        /// <summary>
+        /// Welcomes the user.
+        /// </summary>
+        public void WelcomeMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("WELCOME MESSAGE TO THE USER");
+            Thread.Sleep(1000);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Mainmenu message.
+        /// </summary>
+        public void MainMenuMessage()
+        {
+            Console.WriteLine("Main Menu");
+            Console.ReadLine();
+            Console.WriteLine("1) Go out on adventure!");
+            Console.WriteLine("2) Show Stats");
+            Console.WriteLine("3) Show Rules of Game");
+            Console.WriteLine("4) Exit Game");
+        }
+
+        /// <summary>
+        /// Ruleses the of game.
+        /// </summary>
+        public void RulesOfGame()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("RULES OF THE GAME");
+            Thread.Sleep(1000);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Error message for the menu.
+        /// </summary>
+        public void MenuErrorMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Enter a number between 1-4 to chose an alternative from the menu.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+    }
+}
