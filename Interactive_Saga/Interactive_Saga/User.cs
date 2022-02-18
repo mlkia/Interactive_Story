cec805d4f30a0ed537b2582ad3edc282d9d26119
Malik Alhanouti <50356599+mlkia@users.noreply.github.com>
2022-02-18 12:24:28 +0100

    Add ShowStates method.

--- Interactive_Saga/Interactive_Saga/User.cs
+++ Interactive_Saga/Interactive_Saga/User.cs
@@ -1,18 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Interactive_Saga
+﻿namespace Interactive_Saga
 {
     public class User
     {
-        public string UserName { get; set; }
+        public string UserName { get; set; } = "";
         public int Diamonds { get; set; } = 0;
         public int NumberOfCorrectAnswers { get; set; } = 0;
         public int NumberOfWrongAnswers { get; set; } = 0;
-        public int Level { get; set; }= 1;
+        public int Level { get; set; } = 1;
+
+
+
+        public void ShowStates()
+        {
+            var TheAnswers = new Character();
+
+            int[] numOfAnswers = new int[2];
+
+            numOfAnswers = TheAnswers.AskQuestion();
+
+            NumberOfCorrectAnswers = numOfAnswers[0];
+            NumberOfWrongAnswers = numOfAnswers[1];
+
+            Console.WriteLine("You have " + NumberOfCorrectAnswers + " Right answers");
+            Console.WriteLine("You have " + NumberOfWrongAnswers + " Wrong answers");
+        }
 
     }
 }
