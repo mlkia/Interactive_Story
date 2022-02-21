001cff1ad164e55d54c0a60cb1150c9f63381451
Malik Alhanouti <50356599+mlkia@users.noreply.github.com>
2022-02-21 14:58:02 +0100

    Fixa Programclass conf

--- Interactive_Saga/Interactive_Saga/Program.cs
+++ Interactive_Saga/Interactive_Saga/Program.cs
@@ -1,12 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-namespace SagaÖvning {
-using System;
-
-public static class Program
-{
-    static void Main()
-        {
-        }
-}
-}
