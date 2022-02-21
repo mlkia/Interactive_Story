c5b96a67ec723380fb1c1fbda126b7a4c995f6a9
Josefin Persson <josefinp94@gmail.com>
2022-02-21 09:29:10 +0100

    halvfärdigt, behöver diskuteras i gruppen

---
+++ Interactive_Saga/Interactive_Saga/MessageHelper.cs
@@ -0,0 +1,49 @@
+﻿namespace Interactive_Saga
+{
+    public class MessageHelper
+    {
+        public void WelcomeMessage(UserJosefinTest user)   // spelas upp när man öppnar programmet, frågar om user name och lägger det i user.UserName
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("WELCOME MESSAGE TO THE USER");
+            Thread.Sleep(1000);
+            Console.WriteLine("Tell me your name...");
+            string userName = Console.ReadLine();
+            user.UserName = userName;
+            Console.WriteLine("Hello " + user.UserName);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public void MainMenuMessage()  // huvudmenyn
+        {
+            Console.Clear();
+            Console.WriteLine("Main Menu");
+            Console.ReadLine();
+            Console.WriteLine("1) Go out on adventure!");
+            Console.WriteLine("2) Show Stats");
+            Console.WriteLine("3) Show Rules of Game");
+            Console.WriteLine("4) Exit Game");
+        }
+
+        public void RulesOfGame()  // spelets regler
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("RULES OF THE GAME");
+            Thread.Sleep(1000);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public void MenuErrorMessage() // felmeddelande för meny-switchen
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Enter a number between 1-4 to chose an alternative from the menu.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
