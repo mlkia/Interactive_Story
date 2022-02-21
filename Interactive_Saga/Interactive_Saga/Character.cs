dd7a63668fa55da91ebfc96e3801c6cee844db9c
Ahmedaadam <96ahmada@gafe.molndal.se>
2022-02-21 14:42:48 +0100

    lala

---
+++ Interactive_Saga/Interactive_Saga/Character.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_Saga
+{
+    public abstract class Character
+    {
+       public string Name { get; set; }
+        public List<CharacterQuestion> questions { get; set; }
+
+        //Josefin kod
+     
+    
+        public string Name { get; set; } // karaktärens namn
+        public string HelloPhrase { get; set; }  // hälsningsfras, inte nödvändigt men roligt
+
+        //public string CongratulationsPhrase { get; set; }                  // extra properties vi kan lägga till
+        //public string IncorrectAnswerPhrase { get; set; }
+        //public string GoodbyePhrase { get; set; }
+
+        public Character(string name, string helloPhrase)  // constructor
+        {
+            this.Name = name;
+            this.HelloPhrase = helloPhrase;
+        }
+    }
+    
+}
